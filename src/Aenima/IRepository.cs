0a3e5a5973686bd96d890ac4bedc28ae3c0649e8
Sergio Silveira <sdsilveira@gmail.com>
2015-07-04 17:23:42 +0100

    Changed the name of the aggregate repo

--- src/Aenima/IRepository.cs
+++ src/Aenima/IRepository.cs
@@ -3,7 +3,7 @@ using System.Threading.Tasks;
 
 namespace Aenima
 {
-    public interface IAggregateRepository
+    public interface IRepository
     {
         Task<T> GetById<T>(string identity, int version) where T : class, IAggregate, new();
         Task Save<T>(T aggregate, IDictionary<string, object> headers = null) where T : class, IAggregate;
